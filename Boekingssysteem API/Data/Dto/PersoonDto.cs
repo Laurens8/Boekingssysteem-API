36f3ccca3576783cfdad5bbe84ed5cbe618fbea7
Laurens8 <r0901293@student.thomasmore.be>
2023-05-02 01:17:25 +0200

    dbo aanpassing

--- Boekingssysteem API/Data/Dto/PersoonDto.cs
+++ Boekingssysteem API/Data/Dto/PersoonDto.cs
@@ -7,7 +7,6 @@ namespace Boekingssysteem_API.Data.Dto
         public string Personeelnummer { get; set; }
         public string Naam { get; set; }  
         public string Voornaam { get; set; }  
-        public bool Admin { get; set; }
         public bool? Aanwezig { get; set; }
     }
 }
