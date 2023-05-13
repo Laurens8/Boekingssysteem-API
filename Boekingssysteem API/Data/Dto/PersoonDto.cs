17b256351f938b4b91c9e154991d07b7b7703013
Laurens8 <r0901293@student.thomasmore.be>
2023-05-14 01:15:16 +0200

    admin bugfix

--- Boekingssysteem API/Data/Dto/PersoonDto.cs
+++ Boekingssysteem API/Data/Dto/PersoonDto.cs
@@ -6,7 +6,8 @@ namespace Boekingssysteem_API.Data.Dto
     {
         public string Personeelnummer { get; set; }
         public string Naam { get; set; }  
-        public string Voornaam { get; set; }  
+        public string Voornaam { get; set; }
+        public bool Admin { get; set; }
         public bool? Aanwezig { get; set; }
         public string Wachtwoord { get; set; }
     }
